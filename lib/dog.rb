5f722eeec9cf332cb3b81b8a055441cf5642d270
Michelle Ranly <meranly8@gmail.com>
2020-11-13 03:57:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,10 +14,15 @@ class Dog
         id INTEGER PRIMARY KEY,
         name TEXT,
         breed TEXT
-      )
+      );
     SQL
 
     DB[:conn].execute(sql)
   end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs;"
+  end
+
 
 end
