5e898320febaacb997d0c215edafc83c585c6a70
Michelle Ranly <meranly8@gmail.com>
2020-11-13 04:20:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,4 +65,8 @@ class Dog
     end.first
   end
 
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name: name, breed: breed)
+  end
+
 end
